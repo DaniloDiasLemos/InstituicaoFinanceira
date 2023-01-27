1bd5071a7aa2cf06a0a2f9eff4d005248666077f
Renan Dias <renanvdias@github.com>
2023-01-26 22:10:36 -0300

    Adicionando metodos da classe Transacao

--- InstituicaoFinanceira/src/ContaCorrente.java
+++ InstituicaoFinanceira/src/ContaCorrente.java
@@ -8,4 +8,21 @@ public class ContaCorrente extends Conta {
         this.limiteChequeEspecial = limiteChequeEspecial;
         this.taxaAdministrativa = taxaAdministrativa;
     }
+
+    public double getLimiteChequeEspecial() {
+        return limiteChequeEspecial;
+    }
+
+    public void setLimiteChequeEspecial(double limiteChequeEspecial) {
+        this.limiteChequeEspecial = limiteChequeEspecial;
+    }
+
+    public double getTaxaAdministrativa() {
+        return taxaAdministrativa;
+    }
+
+    public void setTaxaAdministrativa(double taxaAdministrativa) {
+        this.taxaAdministrativa = taxaAdministrativa;
+    }
 }
+
--- InstituicaoFinanceira/src/Transacao.java
+++ InstituicaoFinanceira/src/Transacao.java
@@ -1,10 +1,82 @@
 public class Transacao {
+    private String dataAtual;
+    private String meio;
+    private Conta conta; //Conta informada para realizar saque, depósito, consultar saldo e efetuar pagamento
 
-    private int nroDaConta;
-    private String data;
-
-    public Transacao(int nroDaConta, String data) {
-        this.nroDaConta = nroDaConta;
-        this.data = data;
+    public Transacao(Conta conta, String data) {
+        this.conta = conta;
+        this.dataAtual = data;
     }
+
+    public void sacar (Conta conta, double valor)
+    {
+        //CONTA INATIVA
+        if (!conta.getStatusConta())
+        {
+            System.out.println("Saque negado! Conta Inativa!");
+        }
+        else {
+            //CONTA ATIVA
+            if (conta instanceof ContaPoupanca || conta instanceof ContaSalario) //Não utiliza limite cheque especial
+                if (conta.getSaldoAtual() >= valor) {
+                    conta.setSaldoAtual(conta.getSaldoAtual() - valor);
+                    System.out.println("Saque realizado com sucesso!");
+                }
+
+            if (conta instanceof ContaCorrente)
+                if (conta.getSaldoAtual() + ((ContaCorrente) conta).getLimiteChequeEspecial() >= valor) {
+                    conta.setSaldoAtual(conta.getSaldoAtual() - valor);
+                    System.out.println("Saque realizado com sucesso!");
+                }
+        }
+    }
+
+    public void depositar (Conta conta, double valor)
+    {
+        //CONTA INATIVA
+        if (!conta.getStatusConta())
+        {
+            System.out.println("Depósito negado! Conta Inativa!");
+        } else{
+            //CONTA ATIVA
+            conta.setSaldoAtual(conta.getSaldoAtual() + valor);
+            System.out.println("Depósito realizado com sucesso!");
+        }
+    }
+
+    public void consultarSaldo(Conta conta)
+    {
+        //CONTA INATIVA
+        if (!conta.getStatusConta())
+        {
+            System.out.println("Operação negada! Conta Inativa!");
+        }else{
+            System.out.println("Saldo atual da conta: " + conta.getSaldoAtual());
+        }
+    }
+
+    public void efetuarPagamento (Conta conta, double valor)
+    {
+        //CONTA INATIVA
+        if (!conta.getStatusConta())
+        {
+            System.out.println("Operação negada! Conta Inativa!");
+        }
+        else {
+            //CONTA ATIVA
+            if (conta instanceof ContaPoupanca || conta instanceof ContaSalario) //Não utiliza limite cheque especial
+                if (conta.getSaldoAtual() >= valor) {
+                    conta.setSaldoAtual(conta.getSaldoAtual() - valor);
+                    System.out.println("Pagamento realizado com sucesso!");
+                }
+
+            if (conta instanceof ContaCorrente)
+                if (conta.getSaldoAtual() + ((ContaCorrente) conta).getLimiteChequeEspecial() >= valor) {
+                    conta.setSaldoAtual(conta.getSaldoAtual() - valor);
+                    System.out.println("Pagamento realizado com sucesso!");
+                }
+        }
+    }
+
+
 }
