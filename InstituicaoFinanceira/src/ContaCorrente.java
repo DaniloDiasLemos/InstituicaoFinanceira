3ac21af28eaf5415999dd5973dcbd3a790f6b39e
renanvdias <111937433+renanvdias@users.noreply.github.com>
2023-01-29 15:33:06 -0300

    Consertando Construtor Conta

--- InstituicaoFinanceira/src/ContaCorrente.java
+++ InstituicaoFinanceira/src/ContaCorrente.java
@@ -2,9 +2,8 @@ public class ContaCorrente extends Conta {
     private double limiteChequeEspecial;
     private double taxaAdministrativa;
 
-    public ContaCorrente(int nroDaConta, double saldoAtual, String dataAbertura, String dataUltimaMovimentacao,
-            double limiteChequeEspecial, double taxaAdministrativa, int agenciaBancaria) {
-        super(nroDaConta, saldoAtual, dataAbertura, dataUltimaMovimentacao, agenciaBancaria);
+    public ContaCorrente(double saldoAtual, String dataAbertura, String dataUltimaMovimentacao, AgenciaBancaria agenciaBancaria, Cliente cliente1, double limiteChequeEspecial, double taxaAdministrativa) {
+        super(saldoAtual, dataAbertura, dataUltimaMovimentacao, agenciaBancaria, cliente1);
         this.limiteChequeEspecial = limiteChequeEspecial;
         this.taxaAdministrativa = taxaAdministrativa;
     }
--- InstituicaoFinanceira/src/ContaPoupanca.java
+++ InstituicaoFinanceira/src/ContaPoupanca.java
@@ -1,9 +1,8 @@
 public class ContaPoupanca extends Conta {
     private double rendimentoMesAtual;
 
-    public ContaPoupanca(int nroDaConta, double saldoAtual, String dataAbertura, String dataUltimaMovimentacao,
-            double rendimentoMesAtual, int agenciaBancaria) {
-        super(nroDaConta, saldoAtual, dataAbertura, dataUltimaMovimentacao, agenciaBancaria);
+    public ContaPoupanca(double saldoAtual, String dataAbertura, String dataUltimaMovimentacao, AgenciaBancaria agenciaBancaria, Cliente cliente1, double rendimentoMesAtual) {
+        super(saldoAtual, dataAbertura, dataUltimaMovimentacao, agenciaBancaria, cliente1);
         this.rendimentoMesAtual = rendimentoMesAtual;
     }
 }
